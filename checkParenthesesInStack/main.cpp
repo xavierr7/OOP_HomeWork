b8ec299e615ebc890f0c7e10c9f5e19d6e48ccb2
caesarrrr7 <millerartemka14@gmail.com>
2022-07-25 22:19:43 +0300

    Update main.cpp

--- checkParenthesesInStack/main.cpp
+++ checkParenthesesInStack/main.cpp
@@ -3,9 +3,38 @@
 
 int main()
 {
+	DynamicStack<char, 1024> stack;
 
+	string str = "({x-y-z} * [x + 2y] - (z + 4x));";
+	string incorrectStr;
 
+	for (size_t i = 0; str[i] != ';'; i++)
+	{
+		int tmp;
+		if (str[i] == ')' || str[i] == ']' || str[i] == '}')
+		{
+			tmp = str[i];
 
+			if (tmp == ')')
+				--tmp;
+			else
+				tmp -= 2;
+
+			if (stack.peek() == tmp)
+				stack.pop();
+			else
+				break;
+		}
+		if (str[i] == '(' || str[i] == '[' || str[i] == '{')
+			stack.push(str[i]);
+
+		incorrectStr += str[i];
+	}
+
+	if (!stack.isEmpty())
+		cout << "Your string is incorrect: " << incorrectStr << endl;
+	else
+		cout << "Your string is correct!\n";
 
 	return 0;
 }
