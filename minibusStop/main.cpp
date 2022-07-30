d9480d1da64a7dab7a519eebec4c4b12f91d8132
caesarrrr7 <millerartemka14@gmail.com>
2022-07-30 14:30:51 +0300

    Update main.cpp

--- minibusStop/main.cpp
+++ minibusStop/main.cpp
@@ -1,4 +1,4 @@
-#include "halt.h"
+п»ї#include "halt.h"
 
 int main()
 {
@@ -6,8 +6,8 @@ int main()
 	SetConsoleOutputCP(1251);
 
 	srand(time(NULL));
-	size_t average_Passenger_ArrivalTime = 2; // повинно бути не менше 2
-	size_t average_TimeOfArrival_OfMinibuses = 20; // повинно бути не менше 2
+	size_t average_Passenger_ArrivalTime = 2; // РїРѕРІРёРЅРЅРѕ Р±СѓС‚Рё РЅРµ РјРµРЅС€Рµ 2
+	size_t average_TimeOfArrival_OfMinibuses = 20; // РїРѕРІРёРЅРЅРѕ Р±СѓС‚Рё РЅРµ РјРµРЅС€Рµ 2
 	size_t maximum_NumberOfPeople_AtTheStop_AtOneMoment = 5;
 
 	Halt halt(average_Passenger_ArrivalTime, average_TimeOfArrival_OfMinibuses, maximum_NumberOfPeople_AtTheStop_AtOneMoment);
