ce99624caf5c46faf639d0bbcb2445f9e5626c7b
xavierr7 <107714440+xavierr7@users.noreply.github.com>
2022-07-31 08:30:52 +0300

    Update main.cpp

--- minibusStop/main.cpp
+++ minibusStop/main.cpp
@@ -6,8 +6,9 @@ int main()
 	SetConsoleOutputCP(1251);
 
 	srand(time(NULL));
-	size_t average_Passenger_ArrivalTime = 2; // повинно бути не менше 2
-	size_t average_TimeOfArrival_OfMinibuses = 20; // повинно бути не менше 2
+	size_t average_Passenger_ArrivalTime = 2; // повинно бути не менше 2 по логіці коду
+	size_t average_TimeOfArrival_OfMinibuses = 20; // повинно бути не менше 2 по логіці коду
+                                                       // тому що я їх рандомлю в невеликому діапазоні, щоб трошки позбутися статичності і було більш реалістично  
 	size_t maximum_NumberOfPeople_AtTheStop_AtOneMoment = 5;
 
 	Halt halt(average_Passenger_ArrivalTime, average_TimeOfArrival_OfMinibuses, maximum_NumberOfPeople_AtTheStop_AtOneMoment);
